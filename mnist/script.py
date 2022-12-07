f093a968e9e43c558acd3324f9e6b8b0bd600128
Marcus Kofal <kofalm@CSGP.EDU>
2022-12-07 15:44:07 -0700

    i dunno, tried to convert to csv

--- mnist/script.py
+++ mnist/script.py
@@ -1,12 +1,34 @@
 import gzip
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 
-with open(f"{__file__}\..\\t10k-labels-idx1-ubyte.gz", "rb") as labels, open(f"{__file__}\..\\t10k-images-idx3-ubyte.gz", "rb") as images:
-    imgs, lbls = images.read(), labels.read()
-    imgs = gzip.decompress(imgs)
-    lbls = gzip.decompress(lbls)
+cwd = os.getcwd() + "\\mnist\\"
 
 
+def convert():
+    labels = open(cwd + "t10k-labels.idx1-ubyte", "rb")
+    images = open(cwd + "t10k-images.idx3-ubyte", "rb")
+    csv = open("test-data.csv", "w")
+    images.read(16)
+    labels.read(8)
+    
+    outputs = []
 
-print(lbls[0:40].decode())
+    for i in range(10000):
+        label = ord(labels.read(1))
+        image = []
+        for j in range(28*28):
+            image.append(ord(images.read(1)))
+        outputs += (label, image)
+    
+    for output in outputs:
+        csv.write(",".join(str(p) for p in output)+ "\n")
+    
+    labels.close()
+    images.close()
+    csv.close()
+    
+
+
+convert()
