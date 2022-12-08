0edc919ac84901a330d92dfd041b47e613dfaf1e
Marcus Kofal <kofalm@CSGP.EDU>
2022-12-08 13:31:40 -0700

    implemented reading from csv

--- mnist/display.py
+++ mnist/display.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def display_image(image: str):
-    image = image.split(",")
+    image = image.strip().split(",")
     prediction = image.pop(0)
     image_array = np.asfarray(image[:]).reshape((28,28))
     plt.imshow(image_array, cmap="Greys", interpolation="None")
