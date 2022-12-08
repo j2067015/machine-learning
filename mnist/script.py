69e037c9d3218bb9ce7d721a84904f9f77a6dd5c
Marcus Kofal <kofalm@CSGP.EDU>
2022-12-08 12:09:06 -0700

    converted image and label inputs to csv

--- mnist/script.py
+++ mnist/script.py
@@ -9,21 +9,20 @@ cwd = os.getcwd() + "\\mnist\\"
 def convert():
     labels = open(cwd + "t10k-labels.idx1-ubyte", "rb")
     images = open(cwd + "t10k-images.idx3-ubyte", "rb")
-    csv = open("test-data.csv", "w")
+    csv = open(f"{cwd}test-data.csv", "w")
     images.read(16)
     labels.read(8)
     
     outputs = []
 
     for i in range(10000):
-        label = ord(labels.read(1))
-        image = []
+        image = [ord(labels.read(1))]
         for j in range(28*28):
             image.append(ord(images.read(1)))
-        outputs += (label, image)
-    
-    for output in outputs:
-        csv.write(",".join(str(p) for p in output)+ "\n")
+        outputs.append(image)
+
+    for image in outputs:
+        csv.write(",".join(str(pix) for pix in image)+"\n")
     
     labels.close()
     images.close()
@@ -31,4 +30,3 @@ def convert():
     
 
 
-convert()
